668ff0965577ab29d13d707ecf0ea429f97195fd
Adam7066 <adam20001002@gmail.com>
2022-07-02 16:07:14 +0800

    feat(home): init HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { NLayoutContent } from "naive-ui"
+import { NLayoutContent, NCard, NH1, NH3 } from "naive-ui"
 </script>
 
 <template>
     <n-layout-content>
-        home
+        <n-card m="x-auto t-12" max-w="3xl">
+            <n-h1 font="extrabold">CP_Discussion</n-h1>
+            <n-h3 m="x-12">一個用於<span font="bold">程式設計</span>課程的討論網站</n-h3>
+        </n-card>
     </n-layout-content>
 </template>
