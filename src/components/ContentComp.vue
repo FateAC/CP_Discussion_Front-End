f2a8ff4de7a1f9b74254deee08d842f2d1604332
LoveSnowEx <lsktw22@gmail.com>
2022-07-02 14:09:13 +0800

    style(layout): make background color default

--- src/components/ContentComp.vue
+++ src/components/ContentComp.vue
@@ -14,20 +14,3 @@ import {
 // 
 
 </script>
-<style scoped>
-.n-layout-header,
-.n-layout-footer {
-  background: rgba(128, 128, 128, 0.2);
-  padding: 24px;
-}
-.n-layout-sider {
-  background: rgba(64, 64, 64, 0.3);
-}
-.n-layout-content {
-  background: rgba(64, 64, 64, 0.4);
-}
-
-.n-layout {
-  background: rgba(64, 64, 64, 0.4);
-}
-</style>
