161b5fc1964c905a766611fc41007137d168ab4f
LoveSnowEx <lsktw22@gmail.com>
2022-07-05 23:33:00 +0800

    add(apollo): install package and setup

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router/index"
 import "virtual:windi.css"
+import { DefaultApolloClient } from "@vue/apollo-composable"
+import apolloClient from "./scripts/apolloClient"
 
-createApp(App).use(router).mount("#app")
+createApp(App).use(router).provide(DefaultApolloClient, apolloClient).mount("#app")
