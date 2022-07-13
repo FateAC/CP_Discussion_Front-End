b34d7b18c162aecaba10a1751b349a39c6de7303
Adam7066 <adam20001002@gmail.com>
2022-07-13 15:41:50 +0800

    feat(dashboard): add the menu of user.

--- src/components/UserHomeComp.vue
+++ src/components/UserHomeComp.vue
@@ -1,7 +1,13 @@
 <template>
-	<n-layout> This is the main page for user </n-layout>
+	<n-card m="x-auto t-12" max-w="2xl">
+		<n-h1 font="extrabold">Hello, {{ userName }}</n-h1>
+		<n-h3 m="x-12">在這裡你可以選擇你的課程</n-h3>
+	</n-card>
 </template>
 
 <script setup lang="ts">
-import { NLayout, NH1 } from "naive-ui"
+import { NCard, NH1, NH3 } from "naive-ui"
+import { ref } from "vue"
+
+const userName = ref("User")
 </script>
