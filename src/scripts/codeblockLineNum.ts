c3a143eedbe23b5920e62ec9a652558b1de74ddc
Adam7066 <adam20001002@gmail.com>
2022-07-22 08:13:37 +0800

    style(codeblockLineNum): fix prettier double quotation problem

--- src/scripts/codeblockLineNum.ts
+++ src/scripts/codeblockLineNum.ts
@@ -3,7 +3,7 @@ const lineNum = () => {
 	const pl = pre.length
 	for (let i = 0; i < pl; i++) {
 		pre[i].innerHTML =
-			"<span class=\"line-number\"></span>" + pre[i].innerHTML + "<span class=\"cl\"></span>"
+			"<span class='line-number'></span>" + pre[i].innerHTML + "<span class='cl'></span>"
 		const num = pre[i].innerHTML.split(/\n/).length
 		for (let j = 0; j < num; j++) {
 			const line_num = pre[i].getElementsByTagName("span")[0]
