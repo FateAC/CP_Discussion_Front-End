cb26c4cfc320ece98c5f686a54f6f7527632d884
SubaRya <index20010928@gmail.com>
2022-08-16 01:38:05 +0800

    feat(resetPWD): add loading spin of resetPWD

--- src/components/ResetPasswordComp.vue
+++ src/components/ResetPasswordComp.vue
@@ -1,18 +1,20 @@
 <template>
 	<n-form>
-		<n-form-item label="新密碼">
-			<n-input type="password" v-model:value="newPWD" show-password-on="click" />
-		</n-form-item>
-		<n-form-item label="確認密碼">
-			<n-input type="password" v-model:value="checkPWD" show-password-on="click" />
-		</n-form-item>
-		<n-button w="full" type="info" round @click="resetPWDHandle">重設</n-button>
+		<n-spin v-model:show="resetPasswordShowSpin">
+			<n-form-item label="新密碼">
+				<n-input type="password" v-model:value="newPWD" show-password-on="click" />
+			</n-form-item>
+			<n-form-item label="確認密碼">
+				<n-input type="password" v-model:value="checkPWD" show-password-on="click" />
+			</n-form-item>
+			<n-button w="full" type="info" round @click="resetPWDHandle">重設</n-button>
+		</n-spin>
 	</n-form>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue"
-import { NButton, useMessage, NForm, NFormItem, NInput } from "naive-ui"
+import { NButton, useMessage, NForm, NFormItem, NInput, NSpin } from "naive-ui"
 import { useMutation } from "@vue/apollo-composable"
 import gql from "graphql-tag"
 import bcrypt from "bcryptjs"
@@ -30,22 +32,29 @@ const { mutate: resetPWDMutation, onDone: resetPWDOnDone } = useMutation<string>
 	`
 )
 
-const resetPWDHandle = () => {
+const resetPasswordShowSpin = ref(false)
+
+const resetPWDHandle = async () => {
+	resetPasswordShowSpin.value = true
 	if (newPWD.value == checkPWD.value) {
 		console.log(checkPWD.value)
-		const tmp = bcrypt.hashSync(checkPWD.value, bcrypt.genSaltSync(15))
+		const tmp = await bcrypt.hash(checkPWD.value, bcrypt.genSaltSync(15))
 		console.log(tmp)
 		resetPWDMutation({
-			pwd: bcrypt.hashSync(checkPWD.value, bcrypt.genSaltSync(15)),
+			pwd: tmp,
 		})
 	} else {
+		resetPasswordShowSpin.value = false
 		message.error("請確認密碼是否一致")
 	}
 }
 
 resetPWDOnDone((resultMutation) => {
+	resetPasswordShowSpin.value = false
 	if ((JSON.parse(JSON.stringify(resultMutation?.data ?? ""))["resetPWD"] ?? false) as boolean) {
 		message.success("密碼重設成功")
+		newPWD.value = ""
+		checkPWD.value = ""
 	} else {
 		message.error("密碼重設失敗")
 	}
