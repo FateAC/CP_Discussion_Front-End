01beb1d8abe2653ba8361d1ce56f6f4b7c5133af
LoveSnowEx <lsktw22@gmail.com>
2022-09-02 14:10:17 +0800

    refactor(HomeComp): get username from selfInfo

--- src/components/UserHomeComp.vue
+++ src/components/UserHomeComp.vue
@@ -3,15 +3,12 @@
 		class="bg-transparent bg-gradient-to-r from-viceGreen/65 to-viceBlue/65 rounded-xl py-5 px-5 text-center shadow-lg shadow-black/20 dark:shadow-lg dark:shadow-blue-400/40"
 		m="x-auto t-12"
 		max-w="2xl">
-		<n-h1 font="extrabold">Hello, {{ userName }}</n-h1>
+		<n-h1 font="extrabold">Hello, {{ selfInfo?.username }}</n-h1>
 		<n-h3 m="x-12">在這裡你可以選擇你的課程</n-h3>
 	</n-card>
 </template>
 
 <script setup lang="ts">
 import { NCard, NH1, NH3 } from "naive-ui"
-import { useStore } from "~/scripts/vuex"
-
-const store = useStore()
-const userName = store.state.username
+import { selfInfo } from "~/scripts/login"
 </script>
